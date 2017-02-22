1fa93a8cd12a8600aaf624125509802124f8b9cf
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-02-22 10:13:02 -0500

    A2Q2-added while loops and if statements

--- src/A2Q2.java
+++ src/A2Q2.java
@@ -9,7 +9,6 @@ import becker.robots.Wall;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author simon7323
@@ -24,27 +23,43 @@ public class A2Q2 {
         City kw = new City();
 
         //create new robot in city
-        Robot karel = new Robot(kw,2,2,Direction.EAST);
-        
+        Robot karel = new Robot(kw, 2, 2, Direction.EAST);
+
         //building hurdle race
-        new Wall(kw,2,3,Direction.SOUTH);
-        new Wall(kw,2,3,Direction.EAST);
-        new Wall(kw,2,4,Direction.SOUTH);
-        new Wall(kw,2,4,Direction.EAST);
-        new Wall(kw,2,5,Direction.SOUTH);
-        new Wall(kw,2,6,Direction.SOUTH);
-        new Wall(kw,2,6,Direction.EAST);
-        new Wall(kw,2,7,Direction.SOUTH);
-        new Wall(kw,2,8,Direction.SOUTH);
-        new Wall(kw,2,9,Direction.SOUTH);
-        new Wall(kw,2,9,Direction.EAST);
-        new Wall(kw,2,10,Direction.SOUTH);
-        new Wall(kw,2,11,Direction.SOUTH);
-        
+        new Wall(kw, 2, 3, Direction.SOUTH);
+        new Wall(kw, 2, 3, Direction.EAST);
+        new Wall(kw, 2, 4, Direction.SOUTH);
+        new Wall(kw, 2, 4, Direction.EAST);
+        new Wall(kw, 2, 5, Direction.SOUTH);
+        new Wall(kw, 2, 6, Direction.SOUTH);
+        new Wall(kw, 2, 6, Direction.EAST);
+        new Wall(kw, 2, 7, Direction.SOUTH);
+        new Wall(kw, 2, 8, Direction.SOUTH);
+        new Wall(kw, 2, 9, Direction.SOUTH);
+        new Wall(kw, 2, 9, Direction.EAST);
+        new Wall(kw, 2, 10, Direction.SOUTH);
+        new Wall(kw, 2, 11, Direction.SOUTH);
+
         //create finish line
-        new Thing(kw,2,11);
-        
-        while();
-        
+        new Thing(kw, 2, 11);
+        //create loop
+        while (true) {
+            //if there is a hurdle in front of karel
+            if (!(karel.frontIsClear())) {
+                //avoid wall- turn left
+                karel.turnLeft();
+
+                //if there is no hurdle in front of karel
+            } else {
+                //move forward
+                karel.move();
+                if (karel.frontIsClear()) {
+                    //karel turn right
+                    karel.turnLeft();
+                    karel.turnLeft();
+                    karel.turnLeft();
+                }
+            }
+        } 
     }
 }
