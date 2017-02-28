ca0a3cad4c04ed2aca342acce1b59dc6e6a73ce4
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-02-28 09:41:14 -0500

    A2Q3-completed with comments

--- src/A2Q3.java
+++ src/A2Q3.java
@@ -23,13 +23,14 @@ public class A2Q3 {
         //create new robot in city
         Robot karel = new Robot(kw, 2, 2, Direction.EAST);
 
-        //create loop(karel continues to move since Street is greater than 0)
+        //create loop(while karel is not facing North, turn left)
         while (!(karel.getDirection() == Direction.NORTH)) {
             karel.turnLeft();
         }
 
-
+        // create loop (while the street karel is on is less than equal to zero) 
         while (karel.getStreet() >= 0) {
+        // if the street karel is on equals to zero, turn left and stop
             if (karel.getStreet() == 0) {
                 karel.turnLeft();
                 break;
@@ -37,6 +38,7 @@ public class A2Q3 {
             karel.move();
 
         }
+        // create loop (while the avenue karel is on is greater than zero, karel moves)
         while (karel.getAvenue() > 0) {
             karel.move();
 
